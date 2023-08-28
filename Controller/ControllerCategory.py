0f938fc4745d92380c365a8cf9a392b4f53939da
LuccasFreire <93498458+LuccasFreire@users.noreply.github.com>
2023-08-27 21:16:33 -0300

    Created registerCategory, deleteCategory and changeCategory methods

--- Controller/ControllerCategory.py
+++ Controller/ControllerCategory.py
@@ -5,6 +5,7 @@ sys.path.insert(0, 'C:\\Users\\Lucas Freire\\Documents\\Coding\\Python\\grocery-
 
 # from Model.Category import Category
 from DAO.CategoryDAO import CategoryDAO
+from Model.Category import Category
 
 
 class CategoryController:
@@ -21,6 +22,42 @@ class CategoryController:
             print("Sucessfully registered the category")
         else:
             print("The category already exists in the System!")
+    
+    
+    def deleteCategory(self, categoryToRemove):
+        x = CategoryDAO.read()
+        cat = list(filter(lambda x: x.category == categoryToRemove, x))
+
+        if len(cat) <= 0:
+            print('The category doesnt exists in the System')
+        else:
+            for i in range(len(x)):
+                if x[i].category == categoryToRemove:
+                    del x[i]
+                    break
+            print("Category removed successfully!")
+        
+        with open('categoryRegistry.txt', 'w') as categoryRegistry:
+            for i in x:
+                categoryRegistry.writelines(i.category)
+                categoryRegistry.writelines('\n')
+
+    def changeCategory(self, categoryToChange, newCategory):
+        x = CategoryDAO.read()
+        cat = list(filter(lambda x: x.category == categoryToChange, x))
+        if len(cat) > 0:
+            cat1 = list(filter(lambda x: x.category == categoryToChange, x))
+            if len(cat1) == 0:
+                x = list(map(lambda x: Category(newCategory) if(x.category == categoryToChange) else(x), x))
+
+            else:
+                print("Category you want to change already exists")
+        else:
+            print("Category doesnt exist in the System")
+        with open('categoryRegistry.txt', 'w') as categoryRegistry:
+            for i in x:
+                categoryRegistry.writelines(i.category)
+                categoryRegistry.writelines('\n')
 
 # a = CategoryController()
 # a.registerCategory('Meat')
